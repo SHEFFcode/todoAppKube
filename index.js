5b53d574a40021dad4e2e7a005575404d4ac5ad3
yuriy-shefer-ck <yuriy.shefer@creditkarma.com>
2018-09-22 19:18:30 -0700

    Ability to get a single record

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ const sql = require('mssql')
 
 app.use(bodyParser.json())
 
-app.get('/:id', (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    res.send(id)
+const serverString = `mssql://${USERNAME}:${PASSWORD}@brad-todo-test.database.windows.net/contoso-todo?encrypt=true`
+const startSQL = async () => {
+    await sql.connect(serverString)
+}
+startSQL()
+
+app.get('/:id', async (req, res) => {   
+    const UUID = req.params.id
+    const result = await sql.query`select * from dbo.PrimaryTodo where id=${UUID}`
+    res.send(JSON.stringify(result))
+})
+
+app.get('/', (req, res) => {
+    const getInfo = async () => {
+        try {
+            const result = await sql.query`select * from dbo.PrimaryTodo`
+            res.send(JSON.stringify(result))
+        } catch (err) {
+            console.log(err)
+            res.send(err)
+        }
+    }
+    getInfo()
 })
 
 app.put('/:id', (req, res) => {
@@ -26,26 +45,6 @@ app.delete('/:id', (req, res) => {
     const id = req.params.id
 })
 
-app.get('/', (req, res) => {
-    const getInfo = async () => {
-        try {
-            console.log('Awaiting connection')
-            const serverString = `mssql://${USERNAME}:${PASSWORD}@brad-todo-test.database.windows.net/contoso-todo?encrypt=true`;
-            console.log('connecting...');
-            console.log(serverString);
-            await sql.connect(serverString);
-            console.log('Made the connection')
-            const result = await sql.query`select * from dbo.PrimaryTodo`
-            console.log('Got the result')
-            console.log(result)
-            res.send(JSON.stringify(result))
-        } catch (err) {
-            console.log(err)
-            res.send(err)
-        }
-    }
-  }
-  getInfo()
-})
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
